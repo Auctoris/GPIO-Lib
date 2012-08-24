638f6daf3fe8fd28762e0c8780a507d16be7946a
Auctoris <ajp@auctoris.co.uk>
2012-08-24 11:52:10 +0100

    Fixed checking of the return values

--- gpio_demo.c
+++ gpio_demo.c
@@ -7,25 +7,25 @@ int main()
 	int i;
 
 	for (i=0;i<(sizeof(pins) / sizeof(int));i++)
-		if (!setGPIO_Out(pins[i]))
+		if (setGPIO_Out(pins[i]))
 			return -1;
 
 	for (i=0;i<10;i++)
 	{
-		if (!GPIO_Write(pins[0],0))
+		if (GPIO_Write(pins[0],0))
 			return 1;
-		if (!GPIO_Write(pins[1],1))
+		if (GPIO_Write(pins[1],1))
 			return 1;
 		sleep(1);
-		if (!GPIO_Write(pins[0],1))
+		if (GPIO_Write(pins[0],1))
 			return 1;
-		if (!GPIO_Write(pins[1],0))
+		if (GPIO_Write(pins[1],0))
 			return 1;
 		sleep(1);
 	}
 	
 	for (i=0;i<(sizeof(pins) / sizeof(int));i++)
-		if (!unsetGPIO(pins[i]))
+		if (unsetGPIO(pins[i]))
 			return 2;
 	
 	return 0;
