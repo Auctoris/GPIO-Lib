88f31a4ea1747ddad22e4fd284f5f4b380b980f3
Auctoris <ajp@auctoris.co.uk>
2012-08-24 12:04:58 +0100

    Modified code to work for n-member arrays of pins

--- gpio_demo.c
+++ gpio_demo.c
@@ -5,24 +5,26 @@ int main()
 {
 	int pins[2]={4,21};
 	int i;
+	int c;
+	int j;
 
 	for (i=0;i<(sizeof(pins) / sizeof(int));i++)
 		if (setGPIO_Out(pins[i]))
 			return -1;
 
 	for (i=0;i<10;i++)
-	{
-		if (GPIO_Write(pins[0],0))
-			return 1;
-		if (GPIO_Write(pins[1],1))
-			return 1;
-		sleep(1);
-		if (GPIO_Write(pins[0],1))
-			return 1;
-		if (GPIO_Write(pins[1],0))
-			return 1;
-		sleep(1);
-	}
+		for (c=0;c<(sizeof(pins) / sizeof(int));c++)
+			for (j=0;j<(sizeof(pins) / sizeof(int));j++)
+			{
+					if (c==j)
+						if (GPIO_Write(pins[j],1))
+							return 1;
+					else
+						if (GPIO_Write(pins[j],0))
+							return 1;
+
+					sleep(1);
+			}
 	
 	for (i=0;i<(sizeof(pins) / sizeof(int));i++)
 		if (unsetGPIO(pins[i]))
